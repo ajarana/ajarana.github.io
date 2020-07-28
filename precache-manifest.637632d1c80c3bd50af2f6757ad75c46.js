4e3f0e75812d4569a94e5377398d2e4b7dca578d
Andres Arana <ajoelarana@gmail.com>
2020-07-28 01:31:25 -0400

    Updated

---
+++ precache-manifest.637632d1c80c3bd50af2f6757ad75c46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51db3d8a89146acaeadd8d891bf2342a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db0b017bb08bb0272a6a",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "97e036c1d56783dba516",
+    "url": "/static/css/main.65ffa57c.chunk.css"
+  },
+  {
+    "revision": "db0b017bb08bb0272a6a",
+    "url": "/static/js/2.6b52da85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6b52da85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e036c1d56783dba516",
+    "url": "/static/js/main.c312fc9c.chunk.js"
+  },
+  {
+    "revision": "35ec47dfee4c65c275be",
+    "url": "/static/js/runtime-main.c6e37e88.js"
+  }
+]);
